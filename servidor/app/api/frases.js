6c62f7b28426c12ecccf8ef024179f60a2100af7
Bruno Miguel Moreira Albuquerque <100159869+brunom764@users.noreply.github.com>
2022-05-17 12:57:54 -0300

    Update frases.js

--- servidor/app/api/frases.js
+++ servidor/app/api/frases.js
@@ -3,7 +3,7 @@ var api = {};
 var frases = [
 	{_id: 0, texto:'Ninguém disse que seria fácil mas também ninguém nunca disse que seria tão difícil', tempo: 17 },
 	{_id: 1, texto:'Meu nome é Bond, James Bond',tempo: 5 },
-	{_id: 2, texto:'Foco, força e fé.', tempo: 4 },
+	{_id: 2, texto:'Nunca me diga as probabilidades.', tempo: 4 },
 	{_id: 3, texto:'Carpe diem. Aproveitem o dia, garotos. Façam suas vidas serem extraordinárias.', tempo: 15 },
 	{_id: 4, texto:'Ninguém disse que seria fácil mas também ninguém nunca disse que seria tão difícil.', tempo: 15 },
 	{_id: 5, texto:'É por isso que o Super-Homem trabalha sozinho', tempo: 8 },
